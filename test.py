54aa111d4e5cb60fd2208899060d5d5f327e8ebe
natalka1122 <natalka1122@yandex.ru>
2019-10-27 14:56:07 +0300

    broke the leg at test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 class TestDevOps(unittest.TestCase):
 
     def test_conflict(self):
-        self.assertEqual('Dev' + 'Ops', 'DevOps')
+        self.assertEqual('Dev' + 'Ops', 'Dev_Ops')
 
 if __name__ == '__main__':
     unittest.main()
