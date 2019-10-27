5bdaf0ef866d3e417267a5832dd8baef936da7d5
natalka1122 <natalka1122@yandex.ru>
2019-10-27 14:59:02 +0300

    Fixed en epic bug

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 class TestDevOps(unittest.TestCase):
 
     def test_conflict(self):
-        self.assertEqual('Dev' + 'Ops', 'Dev_Ops')
+        self.assertEqual('Dev' + 'Ops', 'DevOps')
 
 if __name__ == '__main__':
     unittest.main()
